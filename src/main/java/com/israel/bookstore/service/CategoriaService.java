8b3950cd8dc591ca602b09b5d003e596beed3189
israelbraga2 <ibraga1003@gmail.com>
2022-08-08 15:20:16 -0300

    Exceção personalizada para objeto não encontrado

--- src/main/java/com/israel/bookstore/service/CategoriaService.java
+++ src/main/java/com/israel/bookstore/service/CategoriaService.java
@@ -7,16 +7,18 @@ import org.springframework.stereotype.Service;
 
 import com.israel.bookstore.domain.Categoria;
 import com.israel.bookstore.repositories.CategoriaRepository;
+import com.israel.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repository;
-	
+
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
