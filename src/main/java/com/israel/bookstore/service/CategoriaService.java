b3dfbcc222416b175385415a1e7486558cfb2828
israelbraga2 <ibraga1003@gmail.com>
2022-08-08 15:46:33 -0300

    findAll para categoria

--- src/main/java/com/israel/bookstore/service/CategoriaService.java
+++ src/main/java/com/israel/bookstore/service/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.israel.bookstore.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,4 +22,7 @@ public class CategoriaService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
+	public List<Categoria> findAll() {
+		return repository.findAll();
+	}
 }
